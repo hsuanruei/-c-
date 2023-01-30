b3db16a2d3f65043c1a61322e3600abf226f7498
hsuanruei <114502161+hsuanruei@users.noreply.github.com>
2023-01-30 16:42:21 +0800

    Update input output.cpp

--- input output.cpp
+++ input output.cpp
@@ -4,17 +4,17 @@
 int main()
 {
 	//printing data
-	/*
+	
 	std::cout<< "hello c++20 "<< std::endl;
 	
 	int age{16}l;
 	std::cerr <<"error message : somthing is wrong" <<std::endl;
 	std::clog <<" log message : somthing happened" << std::endl
-	*/
+	
 	
 	//DATA INPUT
 	
-	/*
+	
 	int age1;
 	std::string name;
 	
